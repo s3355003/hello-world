c56dff5c4585e7775211d1f00302b7a152a00ee1
Aidan Holmes <s3355003@student.rmit.edu.au>
2017-06-12 09:43:30 +0800

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * 
+ * @author Aidan Holmes
+ * @version 0.1
+ */
+public class HelloWorld
+{
+    //HelloWorld Program
+    public static void main(String[] args){
+        //prints "Hello World" to the terminal window
+        System.out.println();
+        System.out.println("Hello World");
+        System.out.println("This is my second line of text output");
+        System.out.println(); //blank line
+        System.out.println("This is an edit made with Geany");
+        System.out.println("I think I like Geany a bit better");
+        
+        
+        for(int x = 0; x <=5; x++){
+           System.out.println("The value of x is currently: " + x);
+        }
+    }
+}
